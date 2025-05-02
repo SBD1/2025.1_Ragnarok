cfebcda99539ea8ed29e94762e4c6d19ff225fb2
Amanda Cruz Lima <amandacruzlima.ac@gmail.com>
2025-05-02 16:31:25 -0300

    add docusaurus project url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://2025.1-Ragnarok.example.com',
+  url: 'https://sbd1.github.io/2025.1_Ragnarok/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
